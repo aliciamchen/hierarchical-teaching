b72bfaac6a9360c354d6194f4956689a958d2411
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2022-07-24 18:50:13 -0400

    add mushroom grid fn that colors specified area

--- experiments/teacher_2d/js/mushroom-grid.js
+++ experiments/teacher_2d/js/mushroom-grid.js
@@ -1,1 +1,15 @@
-function makeGrid()
+function makeGrid(capThreshold, capDirection, stemThreshold, stemDirection) {
+
+    var stems = stemDirection === 'less' ? _.filter(_.range(1, 9), function(i) {return i < stemThreshold}) : _.filter(_.range(1, 9), function(i) {return i > stemThreshold})
+    var caps = capDirection === 'less' ? _.filter(_.range(1, 9), function(i) {return i < capThreshold}) : _.filter(_.range(1, 9), function(i) {return i > capThreshold})
+
+    stems.forEach((stemVal) => {
+        caps.forEach((capVal) => {
+            $(`#s${stemVal}c${capVal}`).css({
+                "border": "20px solid green"
+            });
+        })
+    })
+
+    return $('#mushroomGrid').html()
+}
