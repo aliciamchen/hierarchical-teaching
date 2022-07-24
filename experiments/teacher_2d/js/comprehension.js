d9ee7e816e94d4dd4c247b825c14394367377ee5
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2022-07-24 13:59:22 -0400

    add comprehension check

--- experiments/teacher_2d/js/comprehension.js
+++ experiments/teacher_2d/js/comprehension.js
@@ -13,25 +13,25 @@ function comprehensionCheck(instructionsParams) {
         type: jsPsychSurveyMultiChoice,
         questions: [
             {
-                prompt: `What are the differences between different students' past island experience?`,
+                prompt: `What are the differences between different students' past experience?`,
                 options: [
-                    'Students will have seen a different number of orange and purple turtles from the island before seeing the new turtles you show them.',
+                    'Students will have seen a different number of tasty and bitter mushrooms from the island before seeing the new mushroom(s) you show them.',
                     'Students will have seen a bunch of different types of islands before seeing the new turtles you show them.',
+                    `Students will know either the cap width or the stem height rule on the island, but not both rules.`,
                     `All students' experience is the same, so I will treat all students the same.`
                 ],
                 required: true
             },
             {
-                prompt: 'How can you earn a high performance bonus?',
+                prompt: 'How can I earn a high performance bonus?',
                 options: [
-                    'By showing my students as few turtles as possible.',
-                    `By not considering what my students have already seen on the island, and instead showing my students ${instructionsParams.maxExamples} turtles (the maximum) on each lesson so I can make sure that the students' guesses are as accurate as possible.`,
-                    `By considering what my students have already seen on the island so that I can correct their assumptions about the island and teach efficiently and accurately/helpfully at the same time.`
+                    `By not considering what my students already know on the island and instead selecting a random mushroom on each lesson.`,
+                    `By considering what my students already know about the island so that I can teach accurately/helpfully.`
                 ],
                 required: true
             },
             {
-                prompt: `Is there a chance you'll encounter the same student twice (not including the students you'll teach over two lessons)?`,
+                prompt: `Is there a chance I'll encounter the same student twice (not including the students I'll teach over two lessons)?`,
                 options: [
                     'Yes, I might encounter the same student twice.',
                     'No, I will teach a <b>new</b> student each time.'
@@ -39,20 +39,11 @@ function comprehensionCheck(instructionsParams) {
                 required: true
             },
             {
-                prompt: `For the students to whom I'll show two sets of turtles (over two lessons), how will my bonus be calculated?`,
+                prompt: `For the students to whom I'll show two sets of mushrooms (over two lessons), how will my bonus be calculated?`,
                 options: [
                     `Based on the student's first guess.`,
                     `Based on the student's second (and final) guess.`,
                     'Based on both guesses.'
-                ],
-                required: true
-            },
-            {
-                prompt: `How will new turtles be shown to my students?`,
-                options: [
-                    'The students will be walked around the island until they see the number of new orange and purple turtles I select for them.',
-                    'The new turtles will be shown to the student as a random sample from the island.',
-                    'The new turtles will appear out of thin air.'
                 ],
                 required: true
             }
@@ -60,7 +51,7 @@ function comprehensionCheck(instructionsParams) {
         randomize_question_order: true,
 
         on_finish: function (data) {
-            data.pass = [data.response.Q0.includes('orange'), data.response.Q1.includes('time'), data.response.Q2.includes('new'), data.response.Q3.includes('final'), data.response.Q4.includes('walked')].every(Boolean)
+            data.pass = [data.response.Q0.includes('stem'), data.response.Q1.includes('accurately'), data.response.Q2.includes('new'), data.response.Q3.includes('final')].every(Boolean)
         }
     }
 }
