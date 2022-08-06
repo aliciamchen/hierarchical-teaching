f98bb2d9622bab7fd92a8fbc521aae507b2ac271
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2022-08-06 12:31:09 -0400

    make comprehension check pretty

--- experiments/teacher_2d/js/comprehension.js
+++ experiments/teacher_2d/js/comprehension.js
@@ -16,7 +16,7 @@ function comprehensionCheck(instructionsParams) {
                 prompt: `What are the differences between different students' past experience?`,
                 options: [
                     'Students will have seen a different number of tasty and bitter mushrooms from the island before seeing the new mushroom(s) you show them.',
-                    'Students will have seen a bunch of different types of islands before seeing the new turtles you show them.',
+                    'Students will have seen a bunch of different types of islands before seeing the new mushroom(s) you show them.',
                     `Students will know either the cap width or the stem height rule on the island, but not both rules.`,
                     `All students' experience is the same, so I will treat all students the same.`
                 ],
@@ -25,8 +25,8 @@ function comprehensionCheck(instructionsParams) {
             {
                 prompt: 'How can I earn a high performance bonus?',
                 options: [
-                    `By not considering what my students already know on the island and instead selecting a random mushroom on each lesson.`,
-                    `By considering what my students already know about the island so that I can teach accurately/helpfully.`
+                    `By selecting a random mushroom on each lesson.`,
+                    `By considering what my students already know about the island so that I can teach accurately and helpfully.`
                 ],
                 required: true
             },
@@ -37,21 +37,12 @@ function comprehensionCheck(instructionsParams) {
                     'No, I will teach a <b>new</b> student each time.'
                 ],
                 required: true
-            },
-            {
-                prompt: `For the students to whom I'll show two sets of mushrooms (over two lessons), how will my bonus be calculated?`,
-                options: [
-                    `Based on the student's first guess.`,
-                    `Based on the student's second (and final) guess.`,
-                    'Based on both guesses.'
-                ],
-                required: true
             }
         ],
         randomize_question_order: true,
 
         on_finish: function (data) {
-            data.pass = [data.response.Q0.includes('stem'), data.response.Q1.includes('accurately'), data.response.Q2.includes('new'), data.response.Q3.includes('final')].every(Boolean)
+            data.pass = [data.response.Q0.includes('stem'), data.response.Q1.includes('accurately'), data.response.Q2.includes('new')].every(Boolean)
         }
     }
 }
