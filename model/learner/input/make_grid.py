b5dff0d303d38a8eeeb90d6343f10de95b62420c
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2022-09-29 15:55:24 -0400

    visualizations with different param vals

--- model/learner/input/make_grid.py
+++ model/learner/input/make_grid.py
@@ -4,7 +4,7 @@ with open("grid.csv", "w") as csv_file:
     writer = csv.writer(csv_file, delimiter=",")
     for speakerAlpha in [1, 2, 4, 8, 16]:
         for listenerAlpha in [1, 2, 4, 8, 16]:
-            for costWeight in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]:
+            for costWeight in [0, 0.1, 0.2, 0.3, 0.4]:
                     writer.writerow(
                         [
                             speakerAlpha,
