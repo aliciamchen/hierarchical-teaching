8473f78e0cdebbedd534bf303c852aa3cb106d67
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2022-11-01 17:02:30 -0400

    move marginalization to do over whole utility

--- model/exp2/simulation.sh
+++ model/exp2/simulation.sh
@@ -1,2 +1,2 @@
 #!/bin/bash#
-webppl simulation.wppl  --require webppl-csv -- --speakerAlpha $1 --listenerAlpha $2 --guessCost $3
+webppl simulation.wppl  --require webppl-csv -- --speakerAlpha $1 --listenerAlpha $2 --guessCost $3 --exampleCost 0.001
