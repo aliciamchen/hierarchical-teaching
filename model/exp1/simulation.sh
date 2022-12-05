456151fbbb6298512783020dafffdc8e5845a96b
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2022-12-05 08:36:05 -0500

    rerun and add model exp1 and exp2 output csvs

--- model/exp1/simulation.sh
+++ model/exp1/simulation.sh
@@ -1,2 +1,2 @@
 #!/bin/bash#
-webppl simulation.wppl --require webppl-csv --random-seed 88 -- --speakerAlpha $1 --costWeight $2
+webppl simulation.wppl --require webppl-csv --random-seed 88 -- --speakerAlpha $1 --exampleCost $2
