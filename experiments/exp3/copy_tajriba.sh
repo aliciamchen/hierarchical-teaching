a84122267059f9b909f2bb217f29b42e49017707
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2023-11-07 19:28:53 -0500

    stuff to periodically copy data

---
+++ experiments/exp3/copy_tajriba.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Generate the current date and time in a format like "YYYYMMDD_HHMMSS"
+current_datetime=$(date +"%Y%m%d_%H%M%S")
+
+# Create a directory with the current date and time as its name
+mkdir -p ~/data/"$current_datetime"
+
+while true; do
+    # Use rsync to copy the tajriba.json file into the newly created folder
+    rsync -avh --progress root@178.128.145.71:~/.empirica/local/tajriba.json data/"$current_datetime"/
+
+    # Sleep for 5 minutes (300 seconds)
+    sleep 300
+done
