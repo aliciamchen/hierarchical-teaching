e73cc4108becdf9e7b592317e9f5da48ca806808
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2023-12-07 19:13:12 -0500

    save data differently to make missed slider values clearer

--- experiments/exp3/client/src/Stage.jsx
+++ experiments/exp3/client/src/Stage.jsx
@@ -34,18 +34,56 @@ export function Stage() {
     learner.round.get("hypothesis_order")
   );
 
-  if (player.stage.get("submit")) {
-    // sometimes this is undefined? why
-    if (players.length === 1) {
-      return <Loading />;
-    }
-
-    return (
-      <div className="text-center text-gray-400 pointer-events-none">
-        Please wait for other player(s).
-      </div>
+  // If timeout last trial, display the values from the trial before
+  function isAllNaN(obj) {
+    return Object.values(obj).every(
+      (value) => value === null || Number.isNaN(value)
     );
   }
+  console.log("slider values so far");
+  console.log(learner.round.get("sliderValuesSoFar").slice(-1)[0]);
+
+
+  function returnLastSliderValues() {
+    // return last slider values if they are not NaN; if the last slider values are all NaN, return the second to last slider values; if the second to last slider values are all NaN, return the third to last slider values
+    // if the third to last slider values are all NaN, return the fourth to last slider values
+    // the fourth to last slider values should never be all NaN
+
+    if (isAllNaN(learner.round.get("sliderValuesSoFar").slice(-1)[0])) {
+      if (isAllNaN(learner.round.get("sliderValuesSoFar").slice(-2)[0])) {
+        if (isAllNaN(learner.round.get("sliderValuesSoFar").slice(-3)[0])) {
+          return learner.round.get("sliderValuesSoFar").slice(-4)[0];
+        } else {
+          return learner.round.get("sliderValuesSoFar").slice(-3)[0];
+        }
+      } else {
+        return learner.round.get("sliderValuesSoFar").slice(-2)[0];
+      }
+    } else {
+      return learner.round.get("sliderValuesSoFar").slice(-1)[0];
+    }
+
+  }
+
+  const lastSliderValues = returnLastSliderValues();
+
+  console.log("Last slider values " + lastSliderValues);
+
+  if (player && player.stage) { // don't know why we have to catch this
+    if (player.stage.get("submit")) {
+      // sometimes this is undefined? why
+      if (players.length === 1) {
+        return <Loading />;
+      }
+
+      return (
+        <div className="text-center text-gray-400 pointer-events-none">
+          Please wait for other player(s).
+        </div>
+      );
+    }
+  }
+
 
   // console.log(player.round.get("selectedCellsSoFar"))
 
@@ -59,7 +97,7 @@ export function Stage() {
           problem_states={round.get("problem")}
           selected_cells={teacher.round.get("selectedCellsSoFar")}
           role={player.get("role")}
-          sliderValues={learner.round.get("sliderValuesSoFar").slice(-1)[0]}
+          sliderValues={lastSliderValues}
         /> // TODO: change slider values based on learner's prev guess
       );
     case "LearnerFeedback":
@@ -71,9 +109,7 @@ export function Stage() {
           problem_states={round.get("problem")}
           selected_cells={teacher.round.get("selectedCellsSoFar")}
           role={player.get("role")}
-          initialSliderValues={
-            learner.round.get("sliderValuesSoFar").slice(-1)[0]
-          } // later change to learner's prev guess aka sliderSoFar
+          initialSliderValues={lastSliderValues} // later change to learner's prev guess aka sliderSoFar
           stageIdx={stage.get("stageIdx")}
         />
       );
@@ -87,7 +123,7 @@ export function Stage() {
           problem_states={round.get("problem")}
           selected_cells={teacher.round.get("selectedCellsSoFar")}
           role={player.get("role")}
-          sliderValues={learner.round.get("sliderValuesSoFar").slice(-1)[0]}
+          sliderValues={lastSliderValues}
         />
       );
     case "ShowRole":
