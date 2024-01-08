cc4c3500c21ee7015e7c1838895a0a22ea07930c
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-01-08 13:49:27 -0500

    add sentry

--- experiments/exp3/client/src/index.jsx
+++ experiments/exp3/client/src/index.jsx
@@ -5,6 +5,11 @@ import "virtual:uno.css";
 import "../node_modules/@empirica/core/dist/player.css";
 import App from "./App";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://e8ab4f6b0e0e6b96fc882a66dac70807@o684533.ingest.sentry.io/4506346270687232",
+});
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
