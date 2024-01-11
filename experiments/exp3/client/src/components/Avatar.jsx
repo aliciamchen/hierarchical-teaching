a6c1c14511512e7b6e8d544accb70d3947196651
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-01-11 18:10:06 -0500

    fix avatar thing

--- experiments/exp3/client/src/components/Avatar.jsx
+++ experiments/exp3/client/src/components/Avatar.jsx
@@ -4,7 +4,7 @@ export function Avatar({ player }) {
   return (
     <img
       className="h-full w-full rounded-md shadow bg-white p-1"
-      src={`https://avatars.dicebear.com/api/identicon/${player.id}.svg`}
+      src={`https://api.dicebear.com/7.x/identicon/svg?seed=${player.id}`}
       alt="Avatar"
     />
   );
