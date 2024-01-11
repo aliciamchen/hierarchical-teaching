ef52c5a6ad689c8625f40716f90e6f088c916186
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-01-11 18:12:13 -0500

    sentry

--- experiments/exp3/client/src/index.jsx
+++ experiments/exp3/client/src/index.jsx
@@ -8,7 +8,23 @@ import "./index.css";
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://e8ab4f6b0e0e6b96fc882a66dac70807@o684533.ingest.sentry.io/4506346270687232",
+  dsn: "https://d02cadb028cd795c016bf442f01f47ed@o4506525893853184.ingest.sentry.io/4506542786412544",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost"],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      maskAllInputs: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 const container = document.getElementById("root");
