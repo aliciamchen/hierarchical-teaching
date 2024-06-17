812dff6f618687e28a16fb79119635b0b4700dbb
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-06-17 14:37:12 -0400

    Update exp1 simulations to include prior weights in trials

--- model/exp1/run_simulation.sh
+++ model/exp1/run_simulation.sh
@@ -1,7 +1,14 @@
 #!/bin/bash#
-export NODE_OPTIONS=--max_old_space_size=30000
-parallel --bar --colsep ',' "sh ./simulation.sh {1} {2}" :::: input/grid.csv
+export NODE_OPTIONS=--max_old_space_size=100000
 
-# combine into one big csv
+mkdir -p output/indiv
+
+node --stack-size=100000 /usr/local/bin/webppl simulation.wppl --require webppl-csv --random-seed 88 -- --speakerAlpha 1 --exampleCost 0.01
+node --stack-size=100000 /usr/local/bin/webppl simulation.wppl --require webppl-csv --random-seed 88 -- --speakerAlpha 2 --exampleCost 0.01
+node --stack-size=100000 /usr/local/bin/webppl simulation.wppl --require webppl-csv --random-seed 88 -- --speakerAlpha 4 --exampleCost 0.01
+node --stack-size=100000 /usr/local/bin/webppl simulation.wppl --require webppl-csv --random-seed 88 -- --speakerAlpha 8 --exampleCost 0.01
+node --stack-size=100000 /usr/local/bin/webppl simulation.wppl --require webppl-csv --random-seed 88 -- --speakerAlpha 16 --exampleCost 0.01
+
+# # combine into one big csv
 head -n 1 output/indiv/sa1_cw0.01.csv > output/combined.out && tail -n+2 -q output/indiv/*.csv >> output/combined.out
 mv output/combined.out output/combined.csv
